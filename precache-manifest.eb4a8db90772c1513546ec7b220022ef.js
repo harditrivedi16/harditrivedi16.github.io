88d42269156c0228485e774b8a01ada19b1dca20
harditrivedi16 <harditrivedi16@gmail.com>
2025-09-05 20:52:43 -0400

    Updates

---
+++ precache-manifest.eb4a8db90772c1513546ec7b220022ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f97ca6b23b32d6234febb5c4e3f3836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca45b816c27f1e17f0a4",
+    "url": "/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "cc7fae863ef92bd23e40",
+    "url": "/static/css/main.f60eae6a.chunk.css"
+  },
+  {
+    "revision": "ca45b816c27f1e17f0a4",
+    "url": "/static/js/2.939fd7ce.chunk.js"
+  },
+  {
+    "revision": "9bf39d5c49166f2a866a40687348a682",
+    "url": "/static/js/2.939fd7ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7fae863ef92bd23e40",
+    "url": "/static/js/main.9d178fb3.chunk.js"
+  },
+  {
+    "revision": "3a807a20a846ad6d2b39",
+    "url": "/static/js/runtime-main.1ecb0ccb.js"
+  },
+  {
+    "revision": "8939f50615df6f20ae29c0253e2c877b",
+    "url": "/static/media/resume.8939f506.pdf"
+  }
+]);
